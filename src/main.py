fb69576392a51050718644fbe720c1be10611e25
wiselychess <wiselychess@gmail.com>
2025-05-30 20:47:11 +0300

    Вроде починил, теперь запускается из корня

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 # src/main.py
-
 from configs.config import config
 
 def main():
     print(f"Starting {config.APP_NAME} in {config.ENV} mode")
-    print(f"Debug mode is {'on' if config.DEBUG else 'off'}")
     print(f"Server will run on port {config.PORT}")
 
 if __name__ == "__main__":
